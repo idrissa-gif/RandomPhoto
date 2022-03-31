91854209cb3a721e3ae6fe961ad493efcaa40622
idrissa-gif <idrissa@iut-dhaka.edu>
2022-03-31 23:38:56 +0600

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by idrissa Mahamoudou Dicko on 31/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
