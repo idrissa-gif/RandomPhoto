077cc12d839e69a65d388eba22276303866c1227
idrissa-gif <idrissa@iut-dhaka.edu>
2022-04-01 08:39:47 +0600

    done with the random photo generator

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -15,14 +15,31 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .white
         return imageView
     }()
+    private let button : UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Generate", for: .normal)
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
         view.addSubview(imageView)
         imageView.frame = CGRect(x:0,y:0, width:300,height:300)
         imageView.center = view.center
+        view.addSubview(button)
+        button.frame = CGRect(x:0,y:0,width:50 ,height:30 )
+        generateRandomPhoto()
     }
-
-
+    func generateRandomPhoto()
+    {
+        let urlstring = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlstring)!
+        guard let data = try? Data(contentsOf: url) else{
+            return
+        }
+        
+        imageView.image = UIImage(data:data)
+    }
 }
 
